d17186cafcad5f386251082c54ef40f88d4c127f
kYasinAblay <58375357+kYasinAblay@users.noreply.github.com>
2019-12-16 03:14:27 +0300

    detaylı Sorgu İşlemleri

---
+++ K.YasinABLAY_Odev1/Form1.Designer.cs
@@ -0,0 +1,402 @@
+﻿namespace K.YasinABLAY_Odev1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblTekSayilarToplami = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblTekSayilarAdedi = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.lblTekSayilarOrtalamasi = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.lblCiftSayilarToplami = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.lblCiftSayilarAdeti = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.lblCiftSayilarOrtalamasi = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtBitisSayisi = new System.Windows.Forms.TextBox();
+            this.txtBaslangicSayisi = new System.Windows.Forms.TextBox();
+            this.label19 = new System.Windows.Forms.Label();
+            this.txtElemanSayisi = new System.Windows.Forms.TextBox();
+            this.label20 = new System.Windows.Forms.Label();
+            this.txtSansliSayi = new System.Windows.Forms.TextBox();
+            this.btnRastgeleSayilariListeyeEkle = new System.Windows.Forms.Button();
+            this.listDizidekiSayilar = new System.Windows.Forms.ListBox();
+            this.listSansliSayininDizidekiIndisleri = new System.Windows.Forms.ListBox();
+            this.listTekSayilar = new System.Windows.Forms.ListBox();
+            this.listCiftSayilar = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Eleman Sayısı :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Başlangıç Sayısı";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 286);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Dizideki Sayılar";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(138, 273);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 34);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Şanslı Sayının \r\ndizideki indisleri";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(255, 286);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Tek Sayılar";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(383, 339);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(161, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Tek Sayılar Toplamı      :";
+            // 
+            // lblTekSayilarToplami
+            // 
+            this.lblTekSayilarToplami.AutoSize = true;
+            this.lblTekSayilarToplami.Location = new System.Drawing.Point(548, 339);
+            this.lblTekSayilarToplami.Name = "lblTekSayilarToplami";
+            this.lblTekSayilarToplami.Size = new System.Drawing.Size(16, 17);
+            this.lblTekSayilarToplami.TabIndex = 6;
+            this.lblTekSayilarToplami.Text = "..";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(383, 370);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(159, 17);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Tek Sayılar Adeti          :";
+            // 
+            // lblTekSayilarAdedi
+            // 
+            this.lblTekSayilarAdedi.AutoSize = true;
+            this.lblTekSayilarAdedi.Location = new System.Drawing.Point(548, 370);
+            this.lblTekSayilarAdedi.Name = "lblTekSayilarAdedi";
+            this.lblTekSayilarAdedi.Size = new System.Drawing.Size(20, 17);
+            this.lblTekSayilarAdedi.TabIndex = 8;
+            this.lblTekSayilarAdedi.Text = "...";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(383, 402);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(159, 17);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Tek Sayılar Ortalaması :";
+            // 
+            // lblTekSayilarOrtalamasi
+            // 
+            this.lblTekSayilarOrtalamasi.AutoSize = true;
+            this.lblTekSayilarOrtalamasi.Location = new System.Drawing.Point(548, 402);
+            this.lblTekSayilarOrtalamasi.Name = "lblTekSayilarOrtalamasi";
+            this.lblTekSayilarOrtalamasi.Size = new System.Drawing.Size(20, 17);
+            this.lblTekSayilarOrtalamasi.TabIndex = 10;
+            this.lblTekSayilarOrtalamasi.Text = "...";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(663, 286);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(75, 17);
+            this.label12.TabIndex = 11;
+            this.label12.Text = "Çift Sayılar";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(760, 339);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(169, 17);
+            this.label13.TabIndex = 12;
+            this.label13.Text = "Çift Sayılar Toplamı        : ";
+            // 
+            // lblCiftSayilarToplami
+            // 
+            this.lblCiftSayilarToplami.AutoSize = true;
+            this.lblCiftSayilarToplami.Location = new System.Drawing.Point(930, 339);
+            this.lblCiftSayilarToplami.Name = "lblCiftSayilarToplami";
+            this.lblCiftSayilarToplami.Size = new System.Drawing.Size(20, 17);
+            this.lblCiftSayilarToplami.TabIndex = 13;
+            this.lblCiftSayilarToplami.Text = "...";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(760, 370);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(167, 17);
+            this.label15.TabIndex = 14;
+            this.label15.Text = "Çift Sayılar Adeti             :";
+            // 
+            // lblCiftSayilarAdeti
+            // 
+            this.lblCiftSayilarAdeti.AutoSize = true;
+            this.lblCiftSayilarAdeti.Location = new System.Drawing.Point(930, 370);
+            this.lblCiftSayilarAdeti.Name = "lblCiftSayilarAdeti";
+            this.lblCiftSayilarAdeti.Size = new System.Drawing.Size(20, 17);
+            this.lblCiftSayilarAdeti.TabIndex = 15;
+            this.lblCiftSayilarAdeti.Text = "...";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(760, 401);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(167, 17);
+            this.label17.TabIndex = 16;
+            this.label17.Text = "Çift Sayılar Ortalaması    :";
+            // 
+            // lblCiftSayilarOrtalamasi
+            // 
+            this.lblCiftSayilarOrtalamasi.AutoSize = true;
+            this.lblCiftSayilarOrtalamasi.Location = new System.Drawing.Point(930, 402);
+            this.lblCiftSayilarOrtalamasi.Name = "lblCiftSayilarOrtalamasi";
+            this.lblCiftSayilarOrtalamasi.Size = new System.Drawing.Size(20, 17);
+            this.lblCiftSayilarOrtalamasi.TabIndex = 17;
+            this.lblCiftSayilarOrtalamasi.Text = "...";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtBitisSayisi);
+            this.groupBox1.Controls.Add(this.txtBaslangicSayisi);
+            this.groupBox1.Controls.Add(this.label19);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(18, 57);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(212, 95);
+            this.groupBox1.TabIndex = 18;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Random Sayı İçin";
+            // 
+            // txtBitisSayisi
+            // 
+            this.txtBitisSayisi.Location = new System.Drawing.Point(123, 60);
+            this.txtBitisSayisi.Name = "txtBitisSayisi";
+            this.txtBitisSayisi.Size = new System.Drawing.Size(77, 22);
+            this.txtBitisSayisi.TabIndex = 22;
+            // 
+            // txtBaslangicSayisi
+            // 
+            this.txtBaslangicSayisi.Location = new System.Drawing.Point(123, 33);
+            this.txtBaslangicSayisi.Name = "txtBaslangicSayisi";
+            this.txtBaslangicSayisi.Size = new System.Drawing.Size(77, 22);
+            this.txtBaslangicSayisi.TabIndex = 21;
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Location = new System.Drawing.Point(38, 60);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(79, 17);
+            this.label19.TabIndex = 20;
+            this.label19.Text = "Bitiş Sayısı ";
+            // 
+            // txtElemanSayisi
+            // 
+            this.txtElemanSayisi.Location = new System.Drawing.Point(141, 22);
+            this.txtElemanSayisi.Name = "txtElemanSayisi";
+            this.txtElemanSayisi.Size = new System.Drawing.Size(80, 22);
+            this.txtElemanSayisi.TabIndex = 19;
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Location = new System.Drawing.Point(56, 176);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(77, 17);
+            this.label20.TabIndex = 20;
+            this.label20.Text = "Şanslı Sayı";
+            // 
+            // txtSansliSayi
+            // 
+            this.txtSansliSayi.Location = new System.Drawing.Point(141, 173);
+            this.txtSansliSayi.Name = "txtSansliSayi";
+            this.txtSansliSayi.Size = new System.Drawing.Size(80, 22);
+            this.txtSansliSayi.TabIndex = 21;
+            // 
+            // btnRastgeleSayilariListeyeEkle
+            // 
+            this.btnRastgeleSayilariListeyeEkle.Location = new System.Drawing.Point(18, 211);
+            this.btnRastgeleSayilariListeyeEkle.Name = "btnRastgeleSayilariListeyeEkle";
+            this.btnRastgeleSayilariListeyeEkle.Size = new System.Drawing.Size(203, 38);
+            this.btnRastgeleSayilariListeyeEkle.TabIndex = 22;
+            this.btnRastgeleSayilariListeyeEkle.Text = "Rastgele Sayıları ListBox\'a At";
+            this.btnRastgeleSayilariListeyeEkle.UseVisualStyleBackColor = true;
+            this.btnRastgeleSayilariListeyeEkle.Click += new System.EventHandler(this.btnRastgeleSayilariListeyeEkle_Click);
+            // 
+            // listDizidekiSayilar
+            // 
+            this.listDizidekiSayilar.ItemHeight = 16;
+            this.listDizidekiSayilar.Location = new System.Drawing.Point(18, 318);
+            this.listDizidekiSayilar.Name = "listDizidekiSayilar";
+            this.listDizidekiSayilar.Size = new System.Drawing.Size(98, 132);
+            this.listDizidekiSayilar.TabIndex = 23;
+            // 
+            // listSansliSayininDizidekiIndisleri
+            // 
+            this.listSansliSayininDizidekiIndisleri.ItemHeight = 16;
+            this.listSansliSayininDizidekiIndisleri.Location = new System.Drawing.Point(138, 318);
+            this.listSansliSayininDizidekiIndisleri.Name = "listSansliSayininDizidekiIndisleri";
+            this.listSansliSayininDizidekiIndisleri.Size = new System.Drawing.Size(98, 132);
+            this.listSansliSayininDizidekiIndisleri.TabIndex = 24;
+            // 
+            // listTekSayilar
+            // 
+            this.listTekSayilar.FormattingEnabled = true;
+            this.listTekSayilar.ItemHeight = 16;
+            this.listTekSayilar.Location = new System.Drawing.Point(258, 318);
+            this.listTekSayilar.Name = "listTekSayilar";
+            this.listTekSayilar.Size = new System.Drawing.Size(98, 132);
+            this.listTekSayilar.TabIndex = 25;
+            // 
+            // listCiftSayilar
+            // 
+            this.listCiftSayilar.ItemHeight = 16;
+            this.listCiftSayilar.Location = new System.Drawing.Point(656, 318);
+            this.listCiftSayilar.Name = "listCiftSayilar";
+            this.listCiftSayilar.Size = new System.Drawing.Size(98, 132);
+            this.listCiftSayilar.TabIndex = 26;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1138, 498);
+            this.Controls.Add(this.listCiftSayilar);
+            this.Controls.Add(this.listTekSayilar);
+            this.Controls.Add(this.listSansliSayininDizidekiIndisleri);
+            this.Controls.Add(this.listDizidekiSayilar);
+            this.Controls.Add(this.btnRastgeleSayilariListeyeEkle);
+            this.Controls.Add(this.txtSansliSayi);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtElemanSayisi);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblCiftSayilarOrtalamasi);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.lblCiftSayilarAdeti);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.lblCiftSayilarToplami);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.lblTekSayilarOrtalamasi);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.lblTekSayilarAdedi);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblTekSayilarToplami);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Ödev-1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblTekSayilarToplami;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblTekSayilarAdedi;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label lblTekSayilarOrtalamasi;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label lblCiftSayilarToplami;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label lblCiftSayilarAdeti;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label lblCiftSayilarOrtalamasi;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtBitisSayisi;
+        private System.Windows.Forms.TextBox txtBaslangicSayisi;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.TextBox txtElemanSayisi;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.TextBox txtSansliSayi;
+        private System.Windows.Forms.Button btnRastgeleSayilariListeyeEkle;
+        private System.Windows.Forms.ListBox listDizidekiSayilar;
+        private System.Windows.Forms.ListBox listSansliSayininDizidekiIndisleri;
+        private System.Windows.Forms.ListBox listTekSayilar;
+        private System.Windows.Forms.ListBox listCiftSayilar;
+    }
+}
+
---
+++ K.YasinABLAY_Odev1/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace K.YasinABLAY_Odev1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int elemanSayisi = 0, baslangicDegeri = 0, bitisDegeri = 0, deger = 0,
+            sayac = 0, SansliSayi = 0;
+        int[] sayilar;
+        private void btnRastgeleSayilariListeyeEkle_Click(object sender, EventArgs e)
+        {
+            int TekSayilarAdeti = 0, TekSayilarToplami = 0, TekSayilarOrtalamasi = 0, CiftSayilarAdeti = 0, CiftSayilarToplami = 0, CiftSayilarOrtalamasi = 0, d = 0;
+            if (int.TryParse(txtElemanSayisi.Text, out elemanSayisi) == false)
+            {
+                MessageBox.Show("Eleman Sayısını Boş Geçemezsiniz.");
+                txtElemanSayisi.Focus();
+                return;
+            }
+            if (int.TryParse(txtBaslangicSayisi.Text, out baslangicDegeri) == false)
+            {
+                MessageBox.Show("Başlangıç Sayısını Boş Geçemezsiniz.");
+                txtBaslangicSayisi.Focus();
+                return;
+            }
+            if (int.TryParse(txtBitisSayisi.Text, out bitisDegeri) == false)
+            {
+                MessageBox.Show("Bitiş Sayısı Boş Geçemezsiniz.");
+                txtBitisSayisi.Focus();
+                return;
+            }
+            listDizidekiSayilar.Items.Clear();
+            listCiftSayilar.Items.Clear();
+            listTekSayilar.Items.Clear();
+            listSansliSayininDizidekiIndisleri.Items.Clear();
+
+
+
+            elemanSayisi = Convert.ToInt32(txtElemanSayisi.Text);
+            if (elemanSayisi == 0)
+            {
+                MessageBox.Show("Eleman sayısını 0'dan farklı giriniz.");
+                return;
+            }
+            baslangicDegeri = Convert.ToInt32(txtBaslangicSayisi.Text);
+            bitisDegeri = Convert.ToInt32(txtBitisSayisi.Text);
+            SansliSayi = Convert.ToInt32(txtSansliSayi.Text);
+            //^---------------Başlangıç kontrolleri------------------^//
+
+
+
+
+            //---------------------İşlemler----------------------//
+
+            Random rnd = new Random();
+            sayilar = new int[elemanSayisi];
+
+
+            if (sayac != elemanSayisi)
+            {
+                for (int i = 0; i < elemanSayisi; i++)
+                {
+                    deger = rnd.Next(baslangicDegeri, bitisDegeri + 1);
+                    sayilar[i] = deger;
+                    listDizidekiSayilar.Items.Add(deger);
+                    sayac++;
+                    //-------Şanslı sayı--------//
+                    if (deger == SansliSayi)
+                    {
+
+                        for (int j = 0; j < i + 1; j++)
+                        {
+
+                            if (sayilar[j] == SansliSayi)
+                            {
+                                if (listSansliSayininDizidekiIndisleri.Items.Contains(j) == false)
+                                {
+                                    listSansliSayininDizidekiIndisleri.Items.Add(j);
+                                }
+                                else
+                                {
+                                    continue;
+                                }
+
+
+
+
+                            }
+
+                        }
+                    }
+
+                    //-------------Tek ve Çift Sayılar Kontrolü-------//
+                    if (deger % 2 == 0)
+                    {
+                        listCiftSayilar.Items.Add(deger);
+
+
+                        CiftSayilarToplami += deger;
+
+                        CiftSayilarAdeti++;
+
+                    }
+                    else
+                    {
+                        listTekSayilar.Items.Add(deger);
+
+
+                        TekSayilarToplami += deger;
+
+                        TekSayilarAdeti++;
+
+                    }
+                }
+            }
+
+
+
+            else
+            {
+                MessageBox.Show("Dizi eleman sayısı doldu.");
+            }
+            //---Çift veya Tek sayı gelmemesi durumuna karşı önlem
+            if (CiftSayilarAdeti != 0)
+            {
+                CiftSayilarOrtalamasi = CiftSayilarToplami / CiftSayilarAdeti;
+
+            }
+            if (TekSayilarAdeti != 0)
+            {
+                TekSayilarOrtalamasi = TekSayilarToplami / TekSayilarAdeti;
+            }
+
+            //---Sonuçlar---//
+            lblTekSayilarToplami.Text = TekSayilarToplami.ToString();
+            lblTekSayilarAdedi.Text = TekSayilarAdeti.ToString();
+            lblTekSayilarOrtalamasi.Text = TekSayilarOrtalamasi.ToString();
+
+            lblCiftSayilarToplami.Text = CiftSayilarToplami.ToString();
+            lblCiftSayilarAdeti.Text = CiftSayilarAdeti.ToString();
+            lblCiftSayilarOrtalamasi.Text = CiftSayilarOrtalamasi.ToString();
+
+
+
+
+            sayac = 0;
+
+        }
+    }
+}
+
